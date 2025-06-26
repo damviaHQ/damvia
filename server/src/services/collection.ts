81f0159c7158439d0dd553ff81eaaf59f4523ecb
Rémi Caumette <remicaumette@icloud.com>
2025-06-26 10:23:50 +0200

    fix(collections): handle null limited_to_group_ids in user access queries

--- server/src/services/collection.ts
+++ server/src/services/collection.ts
@@ -41,7 +41,7 @@ export function userCollectionsQuery(user: User): SelectQueryBuilder<Collection>
 				q = q.orWhere("collection.public IS TRUE")
 			} else if (user.role !== UserRole.GUEST) {
 				q = q.orWhere(
-					"(collection.public IS TRUE AND collection.draft IS FALSE) AND (array_length(collection.limited_to_group_ids, 1) = 0) AND (asset_folder.license_id IS NULL OR (:regionId = ANY(license.allowed_region_ids) AND (license.usage_from IS NULL OR license.usage_from <= now()) AND (license.usage_to IS NULL OR license.usage_to >= now())))",
+					"(collection.public IS TRUE AND collection.draft IS FALSE) AND (coalesce(array_length(collection.limited_to_group_ids, 1), 0) = 0) AND (asset_folder.license_id IS NULL OR (:regionId = ANY(license.allowed_region_ids) AND (license.usage_from IS NULL OR license.usage_from <= now()) AND (license.usage_to IS NULL OR license.usage_to >= now())))",
 					{ regionId: user.regionId },
 				)
 			}
@@ -91,7 +91,7 @@ export function userCollectionFilesQuery(user: User): SelectQueryBuilder<Collect
 				q = q.orWhere("collection.public IS TRUE")
 			} else if (user.role !== UserRole.GUEST) {
 				q = q.orWhere(
-					"(collection.public IS TRUE AND collection.draft IS FALSE) AND (array_length(collection.limited_to_group_ids, 1) = 0) AND (asset_file.license_id IS NULL OR (:regionId = ANY(license.allowed_region_ids) AND (license.usage_from IS NULL OR license.usage_from <= now()) AND (license.usage_to IS NULL OR license.usage_to >= now())))",
+					"(collection.public IS TRUE AND collection.draft IS FALSE) AND (coalesce(array_length(collection.limited_to_group_ids, 1), 0) = 0) AND (asset_file.license_id IS NULL OR (:regionId = ANY(license.allowed_region_ids) AND (license.usage_from IS NULL OR license.usage_from <= now()) AND (license.usage_to IS NULL OR license.usage_to >= now())))",
 					{ regionId: user.regionId }
 				)
 			}
